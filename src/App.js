5d2778c3cef6e431bd9bc1d9e4eac0a59d1c26fd
Lisa Candemyr <lisa_candemyr@hotmail.com>
2024-05-18 16:53:24 +0100

    implemented react.js

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import Navbar from "./Navbar.js";
+import "./App.css";
+
+function App() {
+  return (
+    <div className="App">
+      <div className="grain"></div>
+      <div className="overlay"></div>
+      <Navbar />
+    </div>
+  );
+}
+
+export default App;
