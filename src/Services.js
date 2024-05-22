ede3daa7882363596b416f3adb4ddd3f48511666
Lisa Candemyr <lisa_candemyr@hotmail.com>
2024-05-22 13:35:05 +0100

    made responsive changes to services section

--- src/Services.js
+++ src/Services.js
@@ -1,3 +1,6 @@
+import analysisImg from "./img/analysis.png";
+import evaluationImg from "./img/evaluation.png";
+import supportImg from "./img/support.png";
 import "./Services.css";
 import "./App.css";
 
@@ -6,14 +9,12 @@ return (
     <div className="Services">
       <section className="services" id="services">
        <h3 className="text-center">“Polyurethane is one of the most energy efficient insulation materials available today.”</h3>
-       <p className="services-description text-center">Polyurethane is a highly versatile material that can be used in many far-reaching applications, including off-site construction, house building, floors, and ceiling cavities in both new and retrofit construction projects. PU Technology works closely with its partners to develop bespoke rigid PU foam solutions. From initial project discussions to delivering the correctly specified materials, we have good experience supplying into SIPs panels, cold store panels, refrigerated displays, marine pipelines, molded rigid foam, and more. No matter the complexity of your project, rigid PU/PIR foam materials sourced from PU Technology are available, with a team of application engineers ready to support you and formulate to your specific requirements.</p>
+       <p className="description text-center">Polyurethane is a highly versatile material that can be used in many far-reaching applications, including off-site construction, house building, floors, and ceiling cavities in both new and retrofit construction projects. PU Technology works closely with its partners to develop bespoke rigid PU foam solutions. From initial project discussions to delivering the correctly specified materials, we have good experience supplying into SIPs panels, cold store panels, refrigerated displays, marine pipelines, molded rigid foam, and more. No matter the complexity of your project, rigid PU/PIR foam materials sourced from PU Technology are available, with a team of application engineers ready to support you and formulate to your specific requirements.</p>
       <div className="services">
         <div className="row">
           <div className="analysis col text-center">
              <div className="option-list">
-                <div className="icon-container">
-                  <img src="" alt="" />
-                </div>
+                  <img src={analysisImg} alt="" />
                 <h4>Analysis</h4>
                 <ul>
                   <li><button className="analysis-1 active">Technology Audit</button></li>
@@ -29,9 +30,7 @@ return (
           </div>
           <div className="evaluation col text-center">
             <div className="option-list">
-               <div className="icon-container">
-                 <img src="" alt="" />
-               </div>
+                 <img src={evaluationImg} alt="" />
                <h4>Evaluation</h4>
                <ul>
                  <li><button className="evaluation-1 active">Product & Production Development</button></li>
@@ -47,9 +46,7 @@ return (
           </div>
           <div className="support col text-center">
             <div className="option-list">
-               <div className="icon-container">
-                 <img src="" alt="" />
-               </div>
+                 <img src={supportImg} alt="" />
                <h4>Support</h4>
                <ul>
                  <li><button className="support-1 active">Production Consultation</button></li>
