b7340e0ce4d4c6809d7c5642fdc106c659c99851
Lisa Candemyr <lisa_candemyr@hotmail.com>
2024-05-22 12:04:07 +0100

    made style and responsive changes to intro section

--- src/Introduction.js
+++ src/Introduction.js
@@ -7,15 +7,13 @@ function Introduction() {
     <div className="Introduction">
       <main>
         <div className="row">
-         <div className="col-lg-7">
-          <h1>We are</h1>
-          <p className="main-description-1">Turnkey solutions for <strong>polyurethane</strong> processing equipment.</p>
-          <h2>We offer</h2>
-          <p className="main-description-2">Confidential impartial <strong>analysis</strong>, <strong>evaluation</strong> and <strong>support</strong> in <span>PUR, PIR,</span> hybrid and polyurethane technology.</p>
-         </div>
-         <div className="col-lg-5">
-          <img src={introImg} className="img-fluid" alt="" />
-         </div>
+          <div className="col-lg-7">
+            <h1>We <span>are</span></h1>
+            <h2><span>pu</span>technology</h2>
+          </div>
+          <div className="col-lg-5">
+            <img src={introImg} className="img-fluid" alt="" />
+          </div>
         </div>
       </main>
     </div>
